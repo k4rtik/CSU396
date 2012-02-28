3dee51df41a554d7b256a744a5e1b3fa928f80fd
Kartik Singhal <kartiksinghal@gmail.com>
2012-02-28 06:24:57 +0530

    Initiat commit, with test file ready

---
+++ test.f
@@ -0,0 +1,27 @@
+/* Examples for testing */
+
+true;
+if true then false else true;
+
+succ 5;
+/* pred -2; */
+0; 
+
+succ (pred 0);
+succ (succ 0);
+succ (succ (pred 0));
+pred 0;
+pred (pred 0);
+pred (succ (succ 0));
+
+iszero (pred 0);
+iszero (succ 0);
+iszero (pred (succ 0));
+iszero (succ (pred 0));
+iszero (succ (succ (pred 0)));
+iszero (succ (succ (pred (pred 0))));
+
+/*succ true;*/
+/*pred false;*/
+
+/*if (iszero 0) then pred (succ (pred (succ 0))) else false;*/
