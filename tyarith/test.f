37415b9159da35dcc54fad7ded549adcf9498201
Kartik Singhal <kartiksinghal@gmail.com>
2012-02-28 12:33:11 +0530

    more test cases, with one stuck term

--- tyarith/test.f
+++ tyarith/test.f
@@ -2,7 +2,7 @@
 
 true;
 if true then false else true;
-if false then 4 else 564;
+if (if true then false else true) then 4 else 564;
 
 succ 5;
 /* pred -2; */
@@ -23,6 +23,10 @@ iszero (succ (succ (pred 0)));
 iszero (succ (succ (pred (pred 0))));
 
 if (iszero 1) then pred (succ (pred (succ 0))) else false;
+
+if (if true then false else true) then pred (succ (pred (succ 0))) else false;
+
+if (if (iszero 1) then pred (succ (pred (succ 0))) else false) then false else true;
 
 /*succ true;*/
 /*pred false;*/
