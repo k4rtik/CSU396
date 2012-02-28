e4cc625e0d44114559d56d40028d32889925b108
Kartik Singhal <kartiksinghal@gmail.com>
2012-02-28 06:50:40 +0530

    added pos and neg integers

--- core.ml
+++ core.ml
@@ -7,9 +7,20 @@ open Support.Pervasive
 
 exception NoRuleApplies
 
+let rec ispos t = match t with
+    TmSucc(_,TmZero(_)) -> true
+  | TmSucc(_,t1) -> ispos t1
+  | _ -> false
+  
+let rec isneg t = match t with
+    TmPred(_,TmZero(_)) -> true
+  | TmPred(_,t1) -> isneg t1  
+  | _ -> false  
+
 let rec isnumericval t = match t with
     TmZero(_) -> true
-  | TmSucc(_,t1) -> isnumericval t1
+  | t when ispos t -> true
+  | t when isneg t -> true
   | _ -> false
 
 let rec isval t = match t with
@@ -26,19 +37,19 @@ let rec eval1 t = match t with
   | TmIf(fi,t1,t2,t3) ->
       let t1' = eval1 t1 in
       TmIf(fi, t1', t2, t3)
+  | TmSucc(_,TmPred(_,nv1)) when (isnumericval nv1) ->
+      nv1
+  | TmPred(_,TmSucc(_,nv1)) when (isnumericval nv1) ->
+      nv1
   | TmSucc(fi,t1) ->
       let t1' = eval1 t1 in
       TmSucc(fi, t1')
-  | TmPred(_,TmZero(_)) ->
-      TmZero(dummyinfo)
-  | TmPred(_,TmSucc(_,nv1)) when (isnumericval nv1) ->
-      nv1
   | TmPred(fi,t1) ->
       let t1' = eval1 t1 in
       TmPred(fi, t1')
   | TmIsZero(_,TmZero(_)) ->
       TmTrue(dummyinfo)
-  | TmIsZero(_,TmSucc(_,nv1)) when (isnumericval nv1) ->
+  | TmIsZero(_,nv1) when (ispos nv1 || isneg nv1) ->
       TmFalse(dummyinfo)
   | TmIsZero(fi,t1) ->
       let t1' = eval1 t1 in
